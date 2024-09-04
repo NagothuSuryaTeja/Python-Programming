46bd0b23aa68222c0840ee0df550eeb5ab780038
“NagothuSuryaTeja” <nagothuteja@gmail.com>
2024-09-04 22:10:05 +0530

    Sum of first n Natual Numbers  in python

--- SimplePythonPrograms/Program5_Natural_Number_sum.py
+++ SimplePythonPrograms/Program5_Natural_Number_sum.py
@@ -1,9 +1,9 @@
 def solve():
     n=int(input("enter the number : "))
-    def Sum_of_n_NaturalNubers(n):
+    def Sum_of_n_NaturalNumbers(n):
         Sum = (n*(n+1))/2
         return Sum
-    print(Sum_of_n_NaturalNubers(n))
+    print(Sum_of_n_NaturalNumbers(n))
 t=1
 t=int(input("enter the number of test cases : "))
 for i in range(t):
